32b04e283b723d5fdc68954f2fed8829b9d95de7
sanketh bhongir <sankethbhongir06@gmail.com>
2021-01-29 00:37:04 -0500

    added LRU cache problem

---
+++ LRU_cache/LRU.py
@@ -0,0 +1,13 @@
+class LRU_Cache(object):
+
+    def __init__(self, capacity):
+        # Initialize class variables
+        pass
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent. 
+        pass
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item. 
+        pass
