277a1e9c6e83f8a2aef70178fe4222f911f946f5
sanketh bhongir <sankethbhongir06@gmail.com>
2021-02-08 22:14:23 -0500

    added union and intersection of linked list problem

---
+++ union and intersection linkedlist/union_intersection.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+        return out_string
+
+
+    def append(self, value):
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def size(self):
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+
+        return size
+
+def union(llist_1, llist_2):
+    # Your Solution Here
+    pass
+
+def intersection(llist_1, llist_2):
+    # Your Solution Here
+    pass
